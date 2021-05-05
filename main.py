f50fe1c4c2d785e6c4849061ed18950aed89da28
fesgic <festusgichohi2@gmail.com>
2021-05-05 19:24:17 +0300

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+#netsecurity
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import capture.capture as packets_cap
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    try:
+        packets_cap.packet_capture()
+    except:
+        None
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
