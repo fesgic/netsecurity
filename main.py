ade396bf0d85cbcfa2b1c4594b6c9753215d2199
fesgic <festusgichohi2@gmail.com>
2021-05-31 22:51:50 +0300

    Added support for argument flags

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import sys
 
 import capture.capture as packets_cap
 
+
 if __name__ == '__main__':
     try:
-        packets_cap.packet_capture()
-    except:
-        None
+        packets_cap.file = sys.argv[1].strip()
+        if True:
+            packets_cap.packet_capture()
+    except IndexError:
+        print("[-] Usage: %s <filename to save to>   " % sys.argv[0].strip())
+        print("[-] Example: %s capture.pcap        -    Capturing traffic from the live network" % sys.argv[0].strip())
 
