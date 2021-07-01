de7711b616d6568c7c37d9ae41c165c281ea1503
fesgic <festusgichohi2@gmail.com>
2021-07-01 21:24:14 +0300

    added option to select interface

--- capture/capture.py
+++ capture/capture.py
@@ -7,6 +7,6 @@ from scapy.all import *
 
 def packet_capture():
 
-    pkts = sniff(iface="vmnet8", prn=lambda x: x.summary())
+    pkts = sniff(iface="wlan0", prn=lambda x: x.summary())
     wrpcap(file, pkts)
     print(pkts)
