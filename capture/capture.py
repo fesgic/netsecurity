6020495f154f5fc7608594057643b11beeb23c95
fesgic <festusgichohi2@gmail.com>
2021-07-03 12:45:54 +0300

    select interface, save with rw permissions

--- capture/capture.py
+++ capture/capture.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 logging.getLogger("scapy").setLevel(logging.CRITICAL)
 from scapy.all import *
@@ -7,6 +8,9 @@ from scapy.all import *
 
 def packet_capture():
 
-    pkts = sniff(iface="wlan0", prn=lambda x: x.summary())
+    pkts = sniff(iface=interface, prn=lambda x: x.summary())
     wrpcap(file, pkts)
     print(pkts)
+def permissions():
+    os.system(f"chmod 777 {file}")
+    
